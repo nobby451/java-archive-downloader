9174bd775d24359708ca5eca6bb5f464bc8c8d37
Nobby <nobby451@gmail.com>
2015-09-05 19:21:36 +0900

    Browser ActionにJadeから生成した未実装のbuttonだけがあるpopupを追加する

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp'),
-    yaml = require('gulp-yaml');
+    yaml = require('gulp-yaml'),
+    jade = require('gulp-jade');
 
 gulp.task('default', function () {
   gulp.src('src/*.yml')
       .pipe(yaml())
+      .pipe(gulp.dest('dest'));
+  gulp.src('src/*.jade')
+      .pipe(jade())
       .pipe(gulp.dest('dest'));
 });
